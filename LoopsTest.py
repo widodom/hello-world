9f31cb304f6e91481acabecb6a0fffa1dbc42aab
widodom <indo4film@yahoo.com>
2018-01-24 17:01:08 -0800

    Take 2 - Committing LoopsTest

---
+++ LoopsTest.py
@@ -0,0 +1,15 @@
+
+days=["Monday", "Tuesday", "Wednesday"]
+
+# print(days)
+#this is a "for loop"
+
+for day in days:
+    print("today is "+day)
+
+
+for x in range(1,10):
+    print("the number is {} and the next number is {}".format(x, x+1))
+
+#string format syntax example above
+
